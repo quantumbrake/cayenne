9e73aea28c8840b9f193924f2652791298386ebc
sriki18 <srikiranc@gmail.com>
2018-08-24 12:57:31 +0530

    Add fixture and organize tests in `test_direct_naive`
    Add fixtures for setting up the systems and a class that calls that fixture to test

--- tests/test_direct_naive.py
+++ tests/test_direct_naive.py
@@ -3,131 +3,103 @@
 import numpy as np
 import pytest
 
-from pyssa.pyssa import direct_naive, get_kstoc
+from pyssa.pyssa import direct_naive
 
-def test_null():
+@pytest.fixture
+def setup_basic():
     V_r = np.array([[1,0,0],[0,1,0]])
     V_p = np.array([[0,1,0],[0,0,1]])
     X0 = np.array([100,0,0])
-    k = np.array([0,0])
-    print(V_r, X0, V_r.shape, X0.shape)
-    [_, _, status] = direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
-    assert status == -2
-
-def test_higher_order():
-    V_r = np.array([[2,0,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([100,0,0])
     k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(RuntimeWarning):
-        direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
+    return V_r, V_p, X0, k
 
-def test_too_high_order():
-    V_r = np.array([[2,2,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([100,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
-
-def test_status_3():
-    V_r = np.array([[1,0,0],[0,1,0]])
-    V_p = np.array([[0,0,0],[0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    [_, _, status] = direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
-    assert status == 3
-
-def test_status_2():
-    V_r = np.array([[1,0,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    [_, _, status] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
-    assert status == 2
-
-def test_neg_k():
+@pytest.fixture
+def setup_large():
     V_r = np.array([[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]])
     V_p = np.array([[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1],[0,0,0,0,0]])
     X0 = np.array([10,0,0,0,0])
-    k = np.array([1,1,-1,1,-1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+    k = np.array([1,1,1,1,1])
+    return V_r, V_p, X0, k
 
-def test_Vp_Vr_shape():
-    V_r = np.array([[1,0,0], [0,1,0]])
-    V_p = np.array([[0,1,0]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+class TestSanitize():
 
-def test_kdet_Vr_shape():
-    V_r = np.array([[1,0,0], [0,1,0]])
-    V_p = np.array([[0,1,0], [0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+    def test_null(self):
+        V_r, V_p, X0, k = setup_basic()
+        k = np.array([0,0])
+        [_, _, status] = direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
+        assert status == -2
 
-def test_Vp_neg():
-    V_r = np.array([[1,0,0],[0,1,0]])
-    V_p = np.array([[0,-1,0],[0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+    def test_higher_order(self):
+        V_r, V_p, X0, k = setup_basic()
+        V_r = np.array([[2,0,0],[0,1,0]])
+        with pytest.raises(RuntimeWarning):
+            direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
 
-def test_Vr_neg():
-    V_r = np.array([[-1,0,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+    def test_too_high_order(self):
+        V_r, V_p, X0, k = setup_basic()
+        V_r = np.array([[2,2,0],[0,1,0]])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
 
-def test_X0_neg():
-    V_r = np.array([[1,0,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([-10,0,0])
-    k = np.array([1,1])
-    print(V_r, X0, V_r.shape, X0.shape)
-    with pytest.raises(ValueError):
-        direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+    def test_status_3(self):
+        V_r, V_p, X0, k = setup_basic()
+        V_p = np.array([[0,0,0],[0,0,1]])
+        X0 = np.array([10,0,0])
+        [_, _, status] = direct_naive(V_r, V_p, X0, k, max_t = 10, max_iter = 100)
+        assert status == 3
 
-def test_reproduce():
-    V_r = np.array([[1,0,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    [t1, Xt1, status1] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=0)
-    [t2, Xt2, status2] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=0)
-    flag = 0
-    if Xt1.all() == Xt2.all():
-        if t1 == t2:
-            if status1 == status2:
-                flag = 1
-    assert flag == 1
+    def test_status_2(self):
+        V_r, V_p, X0, k = setup_basic()
+        X0 = np.array([10,0,0])
+        [_, _, status] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+        assert status == 2
 
-def test_reproduce_fail():
-    V_r = np.array([[1,0,0],[0,1,0]])
-    V_p = np.array([[0,1,0],[0,0,1]])
-    X0 = np.array([10,0,0])
-    k = np.array([1,1])
-    [t1, Xt1, status1] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=0)
-    [t2, Xt2, status2] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=1)
-    flag = 0
-    if Xt1.all() == Xt2.all():
-        if t1 == t2:
-            if status1 == status2:
-                flag = 1
-    assert flag == 0
+    def test_neg_k(self):
+        V_r, V_p, X0, k = setup_large()
+        k = np.array([1,1,-1,1,-1])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+
+    def test_Vp_Vr_shape(self):
+        V_r, V_p, X0, k = setup_basic()
+        V_p = np.array([[0,1,0]])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+
+    def test_kdet_Vr_shape(self):
+        V_r, V_p, X0, k = setup_basic()
+        k = np.array([1,1,1])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+
+    def test_Vp_neg(self):
+        V_r, V_p, X0, k = setup_basic()
+        V_p = np.array([[0,-1,0],[0,0,1]])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+
+    def test_Vr_neg(self):
+        V_r, V_p, X0, k = setup_basic()
+        V_r = np.array([[-1,0,0],[0,1,0]])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+
+    def test_X0_neg(self):
+        V_r, V_p, X0, k = setup_basic()
+        X0 = np.array([-10,0,0])
+        with pytest.raises(ValueError):
+            direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100)
+
+    def test_reproduce(self):
+        V_r, V_p, X0, k = setup_basic()
+        [t1, Xt1, status1] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=0)
+        [t2, Xt2, status2] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=0)
+        assert t1 == t2
+        assert Xt1.all() == Xt2.all()
+        assert status1 == status2
+
+    def test_reproduce_fail(self):
+        V_r, V_p, X0, k = setup_basic()
+        [t1, _, _] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=0)
+        [t2, _, _] = direct_naive(V_r, V_p, X0, k, max_t = 1, max_iter = 100, seed=1)
+        assert t1 != t2
