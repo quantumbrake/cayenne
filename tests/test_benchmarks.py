48e60a88ac740e456a6c62b3576fbf2865481bf4
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-03 17:12:46 -0400

    Test: `benchmark` now uses `setup_long` for tests

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -6,11 +6,11 @@ from pyssa.pyssa import direct_naive
 from pyssa.pyssa_cython import cy_direct_naive
 
 
-def test_py_benchmark(benchmark, setup_bifurcation):
-    V_r, V_p, k, X0 = setup_bifurcation
-    benchmark(direct_naive, V_r, V_p, X0, k, max_t=150, max_iter=1000, seed=0)
+def test_py_benchmark(benchmark, setup_long):
+    V_r, V_p, k, X0 = setup_long
+    benchmark(direct_naive, V_r, V_p, X0, k, max_t=1e5, max_iter=1e8, seed=0, chem_flag=True)
 
 
-def test_cy_benchmark(benchmark, setup_bifurcation):
-    V_r, V_p, k, X0 = setup_bifurcation
-    benchmark(cy_direct_naive, V_r, V_p, X0, k, max_t=150, max_iter=1000, seed=0)
+def test_cy_benchmark(benchmark, setup_long):
+    V_r, V_p, k, X0 = setup_long
+    benchmark(cy_direct_naive, V_r, V_p, X0, k, max_t=int(1e5), max_iter=int(1e8), seed=0, chem_flag=True)
