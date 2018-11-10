68495c36fdf2001e045182ef8d222a0f85bf201b
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-10 13:23:20 -0500

    Remove: cython code from package

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -3,18 +3,12 @@
 """
 
 from pyssa.pyssa import direct_naive
-from pyssa.pyssa_cython import cy_direct_naive
 from pyssa.pyssa_numba import numba_direct_naive
 
 
 def test_py_benchmark(benchmark, setup_long):
     V_r, V_p, k, X0 = setup_long
     benchmark(direct_naive, V_r, V_p, X0, k, max_t=1e5, max_iter=1e8, seed=0, chem_flag=True)
-
-
-def test_cy_benchmark(benchmark, setup_long):
-    V_r, V_p, k, X0 = setup_long
-    benchmark(cy_direct_naive, V_r, V_p, X0, k, max_t=int(1e5), max_iter=int(1e8), seed=0, chem_flag=True)
 
 
 def test_numba_benchmark(benchmark, setup_long):
