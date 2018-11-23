938ff5c66fdeb86e9cd4031f3163739258a26ca7
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-23 16:49:08 -0500

    Tests: Change order of returns in `setup_bifurcation` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,7 +55,7 @@ def setup_bifurcation():
     V_p = np.array([[0, 1, 0, 0], [0, 2, 0, 0], [0, 0, 1, 0]])
     k = np.array([1.0, 0.01 * Na, 1.0])
     X0 = np.array([1, 0, 0, 10])
-    return V_r, V_p, k, X0
+    return V_r, V_p, X0, k
 
 
 @pytest.fixture
