e6ce993bf19e1f04509949845fe60ab95a3449c3
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-01 17:16:39 -0500

    Chore: Add `rtd_theme` and `numpydoc` to docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 import pyssa
 
@@ -31,24 +32,24 @@ import pyssa
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "numpydoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'Stochastic Simulation Algorithms in Python'
-copyright = u"2018, Dileep Kishore, Srikiran Chandrasekaran"
-author = u"Dileep Kishore, Srikiran Chandrasekaran"
+project = "Stochastic Simulation Algorithms in Python"
+copyright = "2018, Dileep Kishore, Srikiran Chandrasekaran"
+author = "Dileep Kishore, Srikiran Chandrasekaran"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
@@ -69,10 +70,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -83,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -94,13 +95,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pyssadoc'
+htmlhelp_basename = "pyssadoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -109,15 +110,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -127,9 +125,13 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'pyssa.tex',
-     u'Stochastic Simulation Algorithms in Python Documentation',
-     u'Dileep Kishore, Srikiran Chandrasekaran', 'manual'),
+    (
+        master_doc,
+        "pyssa.tex",
+        "Stochastic Simulation Algorithms in Python Documentation",
+        "Dileep Kishore, Srikiran Chandrasekaran",
+        "manual",
+    )
 ]
 
 
@@ -138,9 +140,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pyssa',
-     u'Stochastic Simulation Algorithms in Python Documentation',
-     [author], 1)
+    (
+        master_doc,
+        "pyssa",
+        "Stochastic Simulation Algorithms in Python Documentation",
+        [author],
+        1,
+    )
 ]
 
 
@@ -150,13 +156,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'pyssa',
-     u'Stochastic Simulation Algorithms in Python Documentation',
-     author,
-     'pyssa',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "pyssa",
+        "Stochastic Simulation Algorithms in Python Documentation",
+        author,
+        "pyssa",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
-
-
-
