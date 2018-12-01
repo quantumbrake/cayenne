8b00190e7757b47805fa1fb22ff76a498764ffc2
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-01 15:05:00 -0500

    Tests: Disable displaying plot in test_plot

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -11,6 +11,5 @@ def test_plotting(setup_basic):
     n_runs = 10
     sim1 = Simulation(V_r, V_p, X0, k)
     sim1.simulate(max_t=150, max_iter=1000, chem_flag=True, n_rep=n_runs)
-    sim1.plot(plot_indices=[0, 1])
+    sim1.plot(plot_indices=[0, 1], disp=False)
     assert 1 == 1
-
