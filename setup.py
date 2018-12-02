72f06d54c31be104bfa7cbcdf908506fa113123e
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-02 13:37:13 -0500

    Chore: Fix README format type

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 import numpy
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 with open("HISTORY.rst") as history_file:
@@ -33,6 +33,7 @@ setup(
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="pyssa",
     name="pyssa",
