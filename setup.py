72a67f7b036939f4e3c80e48b29aec49a9822f87
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-01 19:20:27 -0500

    Chore: Fix version number

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dileep-kishore/pyssa",
-    version="version='0.5.0'",
+    version="0.5.0",
     zip_safe=False,
 )
