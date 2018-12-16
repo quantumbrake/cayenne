fd8936a781a229ffcf9b575582be8869131e80a1
Dileep Kishore <k.dileep1994@gmail.com>
2018-12-16 16:51:02 -0500

    Bump version: 0.5.4 → 0.6.0

--- pyssa/__init__.py
+++ pyssa/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Dileep Kishore, Srikiran Chandrasekaran"""
 __email__ = "k.dileep1994@gmail.com"
-__version__ = "0.5.4"
+__version__ = "0.6.0"
 
 from .simulation import Simulation
