6a93a00ed657671ad4a81b059402c95d9b41840e
Dileep Kishore <k.dileep1994@gmail.com>
2019-03-09 17:51:16 -0500

    Tests: Add tests for `tau_adaptive.step5` function

--- tests/test_adaptive.py
+++ tests/test_adaptive.py
@@ -101,3 +101,19 @@ def test_tauadaptive_step2(setup_basic):
     prop, _, not_crit = step1(kstoc, X0, V_r, V, nc=10)
     taup = step2(not_crit, react_species, V, X0, HOR, prop, epsilon=0.03)
     assert taup == 0.01
+
+
+def test_tauadaptive_step5(setup_basic):
+    V_r, V_p, X0, k = setup_basic
+    nr = V_r.shape[1]
+    V = V_p - V_r
+    kstoc = get_kstoc(V_r, k, 1.0, False)
+    prop, _, not_crit = step1(kstoc, X0, V_r, V, nc=10)
+    taup, taupp = 100, 10
+    tau, K = step5(taup, taupp, nr, not_crit, prop, X0)
+    assert tau == taupp
+    assert K[1] == 0
+    taup, taupp = 0.01, 1
+    tau, K = step5(taup, taupp, nr, not_crit, prop, X0)
+    assert tau == taup
+    assert K[1] == 0
