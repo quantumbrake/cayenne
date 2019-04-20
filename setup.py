d755fdf45491ec81ff072644295ba4d785bdf6ba
Dileep Kishore <k.dileep1994@gmail.com>
2019-04-20 17:01:54 -0400

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Heuro-labs/pyssa",
-    version="0.8.1",
+    version="0.8.2",
     zip_safe=False,
 )
