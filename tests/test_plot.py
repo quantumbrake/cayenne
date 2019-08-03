2caa822192a5576531afca412fd4e135bbf2c521
Dileep Kishore <k.dileep1994@gmail.com>
2019-08-03 16:18:11 -0400

    Tests: Specify algorithm for test_plotting

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -11,7 +11,13 @@ def test_plotting(setup_basic):
     V_r, V_p, X0, k = setup_basic
     n_runs = 10
     sim1 = Simulation(V_r, V_p, X0, k)
-    sim1.simulate(max_t=150, max_iter=1000, chem_flag=True, n_rep=n_runs)
+    sim1.simulate(
+        algorithm="direct_cython",
+        max_t=150,
+        max_iter=1000,
+        chem_flag=True,
+        n_rep=n_runs,
+    )
     sim1.plot(plot_indices=[0, 1], disp=False)
     assert 1 == 1
     with pytest.raises(ValueError):
