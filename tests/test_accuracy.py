2f2ed2a83b0c927e9a18d5dd451718dc73cf37ac
sriki18 <srikiranc@gmail.com>
2019-08-17 13:56:42 -0700

    Add: accuracy test 00021

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -142,3 +142,22 @@ def test_00020(setup_00020, algorithm):
     assert (Z < 3).all()
     assert (-5 < Y).all()
     assert (Y < 5).all()
+
+
+@pytest.mark.parametrize("algorithm", ["direct_cython", "tau_leaping_cython"])
+def test_00021(setup_00021, algorithm):
+    V_r, V_p, X0, k, time_list, mu_list, std_list, max_t, max_iter, n_rep = setup_00021
+    sim1 = Simulation(V_r, V_p, X0, k)
+    sim1.simulate(
+        algorithm=algorithm,
+        max_t=max_t,
+        max_iter=max_iter,
+        chem_flag=False,
+        n_rep=n_rep,
+    )
+    Z, Y = calculate_zy(sim1, time_list, mu_list, std_list)
+    assert np.all(sim1.results.final[0] > time_list[-1])
+    assert (-3 < Z).all()
+    assert (Z < 3).all()
+    assert (-5 < Y).all()
+    assert (Y < 5).all()
