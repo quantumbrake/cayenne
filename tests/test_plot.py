5afbb0b60b779c201918a14c2a796b2b96471c2a
Dileep Kishore <k.dileep1994@gmail.com>
2020-07-04 17:06:07 -0400

    Tests: Update `test_plot` to use `species_names`

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -4,7 +4,6 @@
 
 
 from pyssa.simulation import Simulation
-import pytest
 
 
 def test_plotting(setup_basic):
@@ -15,7 +14,6 @@ def test_plotting(setup_basic):
     sim1.simulate(
         algorithm="direct", max_t=150, max_iter=1000, chem_flag=True, n_rep=n_runs
     )
-    sim1.plot(plot_indices=[0, 1], disp=False)
-    with pytest.raises(ValueError):
-        sim1.plot(plot_indices=[-1], disp=False)
-    sim1.plot(plot_indices=[0, 1], names=["Species A", "Species B"], disp=False)
+    sim1.plot(species_names=["A"])
+    sim1.plot(species_names=["A", "B"], new_names=["Species A", "Secies B"])
+    sim1.plot()
